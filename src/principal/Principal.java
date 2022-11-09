2aeccd052fc6e5092644aee2373e2e4ebdc457ab
ricardo.ayala <ricardo.ayala@habil.mx>
2022-11-09 13:25:53 -0600

    Mi primero commit

---
+++ src/principal/Principal.java
@@ -0,0 +1,21 @@
+package principal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Principal {
+
+    public static void main(String[] args) {
+        List<String> listaNombres = new ArrayList<>();
+        listaNombres.add("juan");
+        listaNombres.add("pedro");
+        listaNombres.add("jose");
+        listaNombres.add("maria");
+        listaNombres.add("sofia");
+        
+        for(int i=0;i<listaNombres.size();i++){
+            System.out.println(listaNombres.get(i).toUpperCase().charAt(0) + listaNombres.get(i).substring(1, listaNombres.get(i).length()));            
+        }
+    }
+}
