45ce0a9b7ee283d3306d44693265a30f790d52ad
ricardo.ayala <ricardo.ayala@habil.mx>
2022-11-09 13:35:19 -0600

    Se agrega el token

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -3,7 +3,6 @@ package principal;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Principal {
 
     public static void main(String[] args) {
@@ -13,9 +12,11 @@ public class Principal {
         listaNombres.add("jose");
         listaNombres.add("maria");
         listaNombres.add("sofia");
-        
-        for(int i=0;i<listaNombres.size();i++){
-            System.out.println(listaNombres.get(i).toUpperCase().charAt(0) + listaNombres.get(i).substring(1, listaNombres.get(i).length()));            
+
+        for (int i = 0; i < listaNombres.size(); i++) {
+            System.out.println(listaNombres.get(i).toUpperCase().charAt(0)
+                    + listaNombres.get(i).substring(1, listaNombres.get(i).length()));
         }
+    //mi token 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
     }
 }
